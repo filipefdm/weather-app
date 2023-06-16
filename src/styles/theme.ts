1f54b74bd7526251db548b6f340548221f5e4e97
Filipe Motta <femotta96@gmail.com>
2023-06-16 15:49:47 -0300

    feat: Add MUI theme configuration

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    mode: "light",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 800,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
+
+export default theme;
