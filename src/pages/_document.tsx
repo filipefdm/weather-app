bde8a7658cae740baea4bd12a1452b681b42bf38
Filipe Motta <femotta96@gmail.com>
2023-06-18 18:07:36 -0300

    refactor: Adjust formatting in Document component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -7,10 +7,10 @@ export default function Document() {
       <body>
         <div
           style={{
-            maxWidth: "1100px",
-            width: "100%",
-            margin: "0 auto",
-            padding: "0 1rem",
+            maxWidth: '1100px',
+            width: '100%',
+            margin: '0 auto',
+            padding: '0 1rem',
           }}
         >
           <Main />
@@ -18,5 +18,5 @@ export default function Document() {
         </div>
       </body>
     </Html>
-  );
+  )
 }
