0fcc3814d751310ebcf1ebe58d207ccff6694f47
Filipe Motta <femotta96@gmail.com>
2023-06-18 09:56:08 -0300

    feat: Create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterContainer } from "./styles";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <p>
+        Developed By <a href="https://github.com/filipefdm">Filipe Motta</a>
+      </p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
