f8c47d0bb3c3d1611fb285b02e570efc8fa4af6d
Filipe Motta <femotta96@gmail.com>
2023-06-18 09:46:06 -0300

    feat: Add e2e test file for Header component in Cypress

---
+++ cypress/e2e/header.spec.ts
@@ -0,0 +1,38 @@
+describe('Header', () => {
+  it('passes', () => {
+    cy.visit({
+      method: 'GET',
+      url: '/',
+      failOnStatusCode: false,
+    })
+
+    // Add a "data-testid" attribute to the title element in the component
+    cy.get('[data-testid="header-title"]').contains('Weather Application') // Check if the title text is correct
+  })
+
+  it('navigates to the GitHub repository when the icon is clicked', () => {
+    cy.visit('http://localhost:3000/')
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/',
+      failOnStatusCode: false,
+    })
+
+    cy.origin('https://github.com', () => {
+      cy.on('uncaught:exception', e => {
+        if (e.message.includes('Things went bad')) {
+          return false
+        }
+      })
+    })
+
+    // Add a "data-testid" attribute to the GitHub link element in the component
+    cy.get('[data-testid="github-link"]').should(
+      'have.attr',
+      'href',
+      'https://www.github.com/filipefdm/weather-app'
+    ) // Check if the href attribute is correct
+
+    cy.get('[data-testid="github-link"] img').click() // Simulate clicking the icon
+  })
+})
