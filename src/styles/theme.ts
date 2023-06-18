9d3a4c6a297dbdd0cf24e1bbe5264188baa39ee0
Filipe Motta <femotta96@gmail.com>
2023-06-18 10:29:36 -0300

    refactor: Refactor interfaces for weather data types

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,29 +1,122 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import lightBg from "../assets/bg.svg";
+import darkBg from "../assets/darkBg.svg";
 
-// Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    mode: "light",
+declare module "@mui/material/styles" {
+  interface Theme {
+    // Adicione suas propriedades personalizadas aqui
+    appTitleColor: string;
+    backgroundImage: string;
+    backgroundGradient: {
+      color1: string;
+      color2: string;
+    };
+    panelBgColor: string;
+    panelTitleColor: string;
+    forecastPanelBgColor: string;
+    searchInput: {
+      color: string;
+      placeholderColor: string;
+    };
+    temperatureSwitch: {
+      backgroundColor: string;
+      sliderColor: string;
+      textColor: string;
+    };
+    searchSuggestion: {
+      backgroundColor: string;
+      hoverBackgroundColor: string;
+      seperatorLineColor: string;
+    };
+    smallIconColor: string;
+    smallIconTextColor: string;
+  }
+
+  // Permite que o tema seja personalizado
+  interface ThemeOptions {
+    appTitleColor?: string;
+    backgroundImage?: string;
+    backgroundGradient?: {
+      color1?: string;
+      color2?: string;
+    };
+    panelBgColor?: string;
+    panelTitleColor?: string;
+    forecastPanelBgColor?: string;
+    searchInput?: {
+      color?: string;
+      placeholderColor?: string;
+    };
+    temperatureSwitch?: {
+      backgroundColor?: string;
+      sliderColor?: string;
+      textColor?: string;
+    };
+    searchSuggestion?: {
+      backgroundColor?: string;
+      hoverBackgroundColor?: string;
+      seperatorLineColor?: string;
+    };
+    smallIconColor?: string;
+    smallIconTextColor?: string;
+  }
+}
+
+// Crie um objeto de tema personalizado estendendo o tema padrão
+const lightTheme = createTheme({
+  appTitleColor: "#2F5D8A",
+  backgroundImage: lightBg,
+  backgroundGradient: {
+    color1: "#F9FFFF",
+    color2: "#38C8E6",
   },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 800,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
+  panelBgColor: "#FFFFFF",
+  panelTitleColor: "#727E8E",
+  forecastPanelBgColor: "rgba(255, 255, 255, 0.75)",
+  searchInput: {
+    color: "#727E8E",
+    placeholderColor: "#6898d5",
   },
+  temperatureSwitch: {
+    backgroundColor: "#77b1c7",
+    sliderColor: "#fff",
+    textColor: "#fff",
+  },
+  searchSuggestion: {
+    backgroundColor: "#fff",
+    hoverBackgroundColor: "#f9f9f9",
+    seperatorLineColor: "#ccc",
+  },
+  smallIconColor: "#A1B9CE",
+  smallIconTextColor: "#7B98B2",
 });
 
-export default theme;
+const darkTheme = createTheme({
+  appTitleColor: "#2F5D8A",
+  backgroundImage: darkBg,
+  backgroundGradient: {
+    color1: "#031027",
+    color2: "#02101D",
+  },
+  panelBgColor: "#051A33",
+  panelTitleColor: "#216397",
+  forecastPanelBgColor: "rgba(5, 26, 51, 0.75)",
+  searchInput: {
+    color: "#5f8bbf",
+    placeholderColor: "#235A84",
+  },
+  temperatureSwitch: {
+    backgroundColor: "#1b3657",
+    sliderColor: "#437abd",
+    textColor: "#718cac",
+  },
+  searchSuggestion: {
+    backgroundColor: "#0f2744",
+    hoverBackgroundColor: "#183553",
+    seperatorLineColor: "#356097",
+  },
+  smallIconColor: "#153C5E",
+  smallIconTextColor: "#3975AB",
+});
+
+export { lightTheme, darkTheme };
--- src/types/weatherTypes.ts
+++ src/types/weatherTypes.ts
@@ -1,83 +1,101 @@
+export interface City {
+  id: number
+  name: string
+  temperature: number
+  description: string
+  isFavorite: boolean
+}
+
 export interface WeatherData {
-  name: string;
-  id: number;
-  weather: Weather[];
+  name: string
+  id: number
+  coord: Coord
+  weather: Weather[]
+  base: string
   main: {
-    temp: number;
-  };
-  humidity: number;
-  windSpeed: number;
-  uvIndex: number;
-  city: string;
-  temperature: number;
-  description: string;
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+    sea_level: number
+    grnd_level: number
+  }
+  windSpeed: Wind
+  humidity: number
+  uvIndex: number
+  city: string
+  temperature: number
+  description: string
 }
 
 export interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
+  id: number
+  main: string
+  description: string
+  icon: string
 }
 
 export interface PositionError extends Error {
-  readonly code: number;
-  readonly message: string;
-  readonly PERMISSION_DENIED: number;
-  readonly POSITION_UNAVAILABLE: number;
-  readonly TIMEOUT: number;
+  readonly code: number
+  readonly message: string
+  readonly PERMISSION_DENIED: number
+  readonly POSITION_UNAVAILABLE: number
+  readonly TIMEOUT: number
 }
 
 export interface CurrentWeather {
-  coord: Coord;
-  weather: Weather[];
-  base: string;
-  main: Main;
-  visibility: number;
-  rain: Rain;
-  clouds: Clouds;
-  dt: number;
-  sys: Sys;
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
+  base?: string
+  clouds?: Clouds
+  coord?: Coord
+  cod?: number
+  dt?: number
+  id?: number
+  main: Main
+  name?: string
+  rain?: Rain
+  sys?: Sys
+  timezone?: number
+  visibility?: number
+  weather?: Weather[]
+  wind?: Wind
 }
 
 interface Coord {
-  lon: number;
-  lat: number;
+  lon: number
+  lat: number
 }
 
 interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-  sea_level: number;
-  grnd_level: number;
+  temp: number
+  feels_like: number
+  temp_min: number
+  temp_max: number
+  pressure: number
+  humidity: number
+  sea_level: number
+  grnd_level: number
 }
 
 export interface Wind {
-  speed: number;
-  deg: number;
-  gust: number;
+  speed: number
+  deg: number
+  gust: number
 }
 
 export interface Rain {
-  "1h": number;
+  '1h': number
 }
 
 interface Clouds {
-  all: number;
+  all: number
 }
 
 interface Sys {
-  type: number;
-  id: number;
-  country: string;
-  sunrise: number;
-  sunset: number;
+  type: number
+  id: number
+  country: string
+  sunrise: number
+  sunset: number
 }
