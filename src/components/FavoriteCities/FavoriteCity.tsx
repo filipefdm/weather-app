1350287b45c9a9af166884a4b54e7bb8325503cb
Filipe Motta <femotta96@gmail.com>
2023-06-18 09:53:58 -0300

    feat: Create FavoriteCity component

---
+++ src/components/FavoriteCities/FavoriteCity.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Typography } from '@mui/material'
+import { City } from '../../types/weatherTypes'
+import { useDispatch } from 'react-redux'
+import { removeFavorite } from '../../store/slices/favoritesSlice'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+interface FavoriteCityProps {
+  favorite: City
+  onRemoveFavorite: (city: City) => void
+}
+
+export const FavoriteCity: React.FC<FavoriteCityProps> = ({ favorite }) => {
+  const dispatch = useDispatch()
+
+  const handleRemoveFavorite = () => {
+    dispatch(removeFavorite(favorite.id))
+  }
+
+  return (
+    <Box
+      key={favorite.id}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: 1,
+        padding: 2,
+        borderRadius: 4,
+        boxShadow: 1,
+        bgcolor: '#f8f8f8',
+      }}
+    >
+      <Typography variant="body1" sx={{ marginRight: 2 }}>
+        {favorite.name}
+      </Typography>
+      <Button
+        variant="outlined"
+        color="error"
+        onClick={handleRemoveFavorite}
+        sx={{ marginLeft: 'auto' }}
+      >
+        <DeleteIcon />
+      </Button>
+    </Box>
+  )
+}
