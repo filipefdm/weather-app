29952a701576d36b83b7d49cb9df70166111d563
Filipe Motta <femotta96@gmail.com>
2023-06-18 10:24:01 -0300

    refactor: Refactor Redux store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import currentCoordinatesReducer from "./slices/currentCoordinatesSlice";
-import currentWeatherReducer from "./slices/currentWeatherSlice";
-import searchHistoryReducer from "./slices/searchHistorySlice";
+import { configureStore } from '@reduxjs/toolkit'
+
+import appReducer from './slices/appReducer'
+import favoritesSliceReducer from './slices/favoritesSlice'
+import searchHistoryReducer from './slices/searchHistorySlice'
+import searchBarReducer from './slices/searchBarSlice'
 
 export const store = configureStore({
   reducer: {
-    currentWeather: currentWeatherReducer,
-    currentCoordinates: currentCoordinatesReducer,
+    app: appReducer,
+    favorites: favoritesSliceReducer,
     searchHistory: searchHistoryReducer,
+    searchBar: searchBarReducer,
   },
-});
+})
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
