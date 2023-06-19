4289f0478835a520e796977c29e7b89a45bef665
Filipe Motta <femotta96@gmail.com>
2023-06-19 02:49:17 -0300

    chore: Update e2e tests for searchHistory component

--- cypress/e2e/searchHistory.spec.ts
+++ cypress/e2e/searchHistory.spec.ts
@@ -1,6 +1,6 @@
 describe('SearchHistory Component', () => {
   beforeEach(() => {
-    cy.visit('/') // Altere a rota conforme necessário
+    cy.visit('/')
   })
 
   it('should display the search history section title', () => {
@@ -21,11 +21,12 @@ describe('SearchHistory Component', () => {
 
     cy.get('[data-testid="search-input"]').type(cityName)
     cy.get('[data-testid="submit-button"]').click()
-    cy.location('href').should('include', `/search?city=${cityName}`)
-    // Adicione mais verificações ou asserções necessárias para testar o resultado da pesquisa
   })
 
   it('should display the search history items', () => {
+    const cityName = 'New York'
+
+    cy.get('[data-testid="search-input"]').type(cityName)
     cy.get('[data-testid="search-history-item"]').should(
       'have.length.greaterThan',
       0
@@ -33,18 +34,25 @@ describe('SearchHistory Component', () => {
   })
 
   it('should trigger the onSearch callback when clicking on the "Exibir" button', () => {
+    const cityName = 'New York'
+
+    cy.get('[data-testid="search-input"]').type(cityName)
+    cy.get('[data-testid="submit-button"]').click()
     cy.get('[data-testid="search-history-item"]')
       .first()
       .contains('Exibir')
       .click()
-    // Faça asserções ou verificações adicionais necessárias após o clique no botão "Exibir"
   })
 
   it('should trigger the onAddFavorite callback when clicking on the Star button', () => {
-    cy.get('[data-testid="search-history-item"]')
-      .first()
-      .contains('Adicionar aos Favoritos')
-      .click()
-    // Faça asserções ou verificações adicionais necessárias após o clique no botão "Adicionar aos Favoritos"
+    const cityName = 'New York'
+
+    cy.get('[data-testid="search-input"]').type(cityName)
+    cy.get('[data-testid="submit-button"]').click()
+    cy.get('[data-testid="search-history-item"]').should(
+      'have.length.greaterThan',
+      0
+    )
+    cy.get('[data-testid="star-icon"]').click()
   })
 })
