72623a673576162c5a3b2d33c2fe32a9d9992011
Filipe Motta <femotta96@gmail.com>
2023-06-19 10:07:54 -0300

    chore: Update MyApp component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
-import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import { Poppins } from 'next/font/google'
+import '../styles/globals.css'
 
+import { CacheProvider } from '@emotion/react'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
-import { CacheProvider } from '@emotion/react'
 
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 
-import { lightTheme, darkTheme } from '../styles/theme'
+import { lightTheme } from '../styles/theme'
 
 import createEmotionCache from '../lib/createEmotionCache'
 
-const poppins = Poppins({ subsets: ['latin'], weight: '300' })
+const poppins = Poppins({ subsets: ['latin'], weight: '200' })
 
 const cache = createEmotionCache()
 
@@ -21,9 +21,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <CacheProvider value={cache}>
-        <ThemeProvider
-          theme={store.getState().app.darkMode ? darkTheme : lightTheme}
-        >
+        <ThemeProvider theme={lightTheme}>
           <CssBaseline />
           <main className={poppins.className}>
             <Component {...pageProps} />
