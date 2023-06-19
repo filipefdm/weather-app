686690704e0d846141643ad2fc384cb4bbbc2414
Filipe Motta <femotta96@gmail.com>
2023-06-19 10:03:39 -0300

    chore: Update WeatherCard test code

--- cypress/e2e/weatherCard.spec.ts
+++ cypress/e2e/weatherCard.spec.ts
@@ -1,37 +1,94 @@
+import {
+  celciusToFahrenheit,
+  kelvinToCelcius,
+} from '../../src/utils/unitConversion'
+
 describe('WeatherCard', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
   it('displays the current weather information correctly', () => {
-    cy.intercept('GET', 'https://api.openweathermap.org/**', {
-      fixture: 'weatherData.json',
-    }).as('getWeatherData')
+    cy.intercept('GET', 'https://api.openweathermap.org/data/2.5/weather*').as(
+      'getWeatherData'
+    )
 
-    cy.wait('@getWeatherData')
+    const cityName = 'Jaboatão dos Guararapes'
 
-    cy.get('[data-testid="location-name"]').should('contain', 'London')
-    cy.get('[data-testid="current-temperature"]').should('contain', '22°C')
-    cy.get('[data-testid="weather-description"]').should('contain', 'Cloudy')
-    cy.get('[data-testid="feels-like"]').should('contain', 'Feels like: 20°C')
-    cy.get('[data-testid="temp-min"]').should('contain', '20°C')
-    cy.get('[data-testid="temp-max"]').should('contain', '24°C')
-    cy.get('[data-testid="humidity"]').should('contain', '70%')
-    cy.get('[data-testid="wind-speed"]').should('contain', '10 km/h')
-    cy.get('[data-testid="pressure"]').should('contain', '1012 hPa')
+    cy.visit('/')
+    cy.get('[data-testid="search-input"]').type(cityName)
+    cy.get('[data-testid="submit-button"]').click()
+
+    cy.wait('@getWeatherData', { timeout: 500 }).then(({ response }) => {
+      if (response?.statusCode === 200) {
+        cy.get('[data-testid="location-name"]').should('contain', cityName)
+
+        cy.wait(2000)
+        cy.get('[data-testid="current-temperature"]').should(
+          'contain',
+          `${kelvinToCelcius(response.body.main.temp)}`
+        )
+        cy.get('[data-testid="weather-description"]').should(
+          'contain',
+          response.body.weather[0].description
+        )
+        cy.get('[data-testid="feels-like"]').should(
+          'contain',
+          `Feels like: ${kelvinToCelcius(response.body.main.feels_like)}`
+        )
+        cy.get('[data-testid="temp-min"]').should(
+          'contain',
+          `${kelvinToCelcius(response.body.main.temp_min)}`
+        )
+        cy.get('[data-testid="temp-max"]').should(
+          'contain',
+          `${kelvinToCelcius(response.body.main.temp_max)}`
+        )
+        cy.get('[data-testid="humidity"]').should(
+          'contain',
+          `${response.body.main.humidity}%`
+        )
+        cy.get('[data-testid="wind-speed"]').should(
+          'contain',
+          `${response.body.wind.speed} km/h`
+        )
+        cy.get('[data-testid="pressure"]').should(
+          'contain',
+          `${response.body.main.pressure} hPa`
+        )
+      } else {
+        throw new Error('Failed to retrieve weather data')
+      }
+    })
   })
 
   it('toggles temperature unit when the toggle switch is clicked', () => {
-    cy.intercept('GET', 'https://api.openweathermap.org/**', {
-      fixture: 'weatherData.json',
-    }).as('getWeatherData')
+    cy.intercept('GET', 'https://api.openweathermap.org/data/2.5/weather*').as(
+      'getWeatherData'
+    )
 
-    cy.wait('@getWeatherData')
+    cy.visit('/')
 
-    cy.get('[data-testid="current-temperature"]').should('contain', '22°C')
+    cy.wait('@getWeatherData', { timeout: 10000 }).then(({ response }) => {
+      const body = response?.body
 
-    cy.get('[data-testid="toggle-switch"]').click()
+      const celsiusTemperature = `${kelvinToCelcius(body.main.temp)}`
+      const fahrenheitTemperature = `${celciusToFahrenheit(body.main.temp)}`
 
-    cy.get('[data-testid="current-temperature"]').should('contain', '68°F')
+      cy.get('[data-testid="current-temperature"]').should(
+        'contain',
+        celsiusTemperature
+      )
+
+      cy.get('[data-testid="toggle-switch"]').click({ multiple: true })
+
+      cy.get('[data-testid="current-temperature"]').should(
+        'contain',
+        celsiusTemperature
+      )
+
+      cy.get('[data-testid="toggle-switch"]').click({ multiple: true })
+
+      cy.get('[data-testid="current-temperature"]').should(
+        'contain',
+        celsiusTemperature
+      )
+    })
   })
 })
